2948443b5eee5236c3cd436185ac583072800d04
esc <valentin@esc.in-berlin.de>
2009-02-14 04:43:58 +0100

    parsing single line entries

--- card.py
+++ card.py
@@ -1,16 +1,55 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-class card(object):
-    self.id = str()
-    self.set = str()
-    self.copyright = str()
-    self.name = str()
-    self.type = str()
-    self.cost = int()
-    self.rarity = str()
-    self.text = str()
-    self.ruling = str()
-    self.flavor = str()
-    self.artist = str()
-   
+id =        "id"
+set =       "set"
+copyright = "copyright"
+name =      "name"
+type =      "type"
+cost =      "cost"
+rarity =    "ruling"
+text =      "text"
+ruling =    "ruling"
+flavor =    "flavor"
+artist =    "artist"
+
+
+class card(dict):
+    pass
+
+class mws_parser():
+
+    def __init__(self):
+        self.cardlist = []
+
+    def parse_mws(self,filename):
+        file = open(filename)
+        self.set = self._parse_single(file,name)
+        while True :
+            id_val = self._parse_single(file,id)
+            if id_val == "" :
+                return self.cardlist
+
+            c = card()
+            c[id] = id_val
+            c[set] = self.set
+            c[copyright] = self._parse_single(file,copyright)
+            c[name] = self._parse_single(file,name)
+            c[type] = self._parse_single(file,type)
+            self.cardlist.append(c)
+
+    def _parse_single(self, file, string):
+        while True :
+            l = file.readline()
+            if l == "" :
+                return ""
+
+            if string+"=" in l:
+                start = l.find(string)+len(string)+2
+                end = l.find('"',start)
+                return l[start:end]
+        
+if __name__ == '__main__':
+    p = mws_parser()
+    cardlist = p.parse_mws('Proteus.set')
+    print cardlist
