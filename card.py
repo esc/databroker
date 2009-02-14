2fb6c6a42d640fda32949e64f76be10e620ee8a0
esc <valentin@esc.in-berlin.de>
2009-02-14 02:34:50 +0100

    adding basic card datastructure

--- card.py
+++ card.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 #coding=utf-8
 
+class card(object):
+    self.id = str()
+    self.set = str()
+    self.copyright = str()
+    self.name = str()
+    self.type = str()
+    self.cost = int()
+    self.rarity = str()
+    self.text = str()
+    self.ruling = str()
+    self.flavor = str()
+    self.artist = str()
+   
